b99369691c9b14a60c3f3040114c6079abf13ae7
ren <ren_fujiwara@serio-toyo.co.jp>
2020-10-14 15:02:22 +0900

    Nodeサーバー構築

---
+++ server/fake-db.js
@@ -0,0 +1,75 @@
+const Product = require('./model/product')
+
+class FakeDb {
+    constructor() {
+        this.products = [
+            {
+                coverImage: './assets/img/phone-cover.jpg',
+                name: 'Phone XL',
+                price: 799,
+                description: 'A large phone with one of the best screens',
+                heading1: 'サンプル１',
+                heading2: 'サンプル２',
+                heading3: 'サンプル３',
+                headingtext1: 'Donec sed odio dui. Etiam porta sem malesuada magna mollis euismod. Nullam id dolor id nibh ultricies vehicula ut id elit. Morbi leo risus, porta ac consectetur ac, vestibulum at eros. Praesent commodo cursus magna.',
+                headingtext2: 'Duis mollis, est non commodo luctus, nisi erat porttitor ligula, eget lacinia odio sem nec elit. Cras mattis consectetur purus sit amet fermentum. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh.',
+                headingtext3: 'Donec sed odio dui. Cras justo odio, dapibus ac facilisis in, egestas eget quam. Vestibulum id ligula porta felis euismod semper. Fusce dapibus, tellus ac cursus commodo, tortor mauris condimentum nibh, ut fermentum massa justo sit amet risus.'
+            },  
+            {
+                coverImage: './assets/img/phone-cover.jpg',
+                name: 'Phone Mini',
+                price: 699,
+                description: 'A great phone with one of the best cameras',
+                heading1: 'サンプル１',
+                heading2: 'サンプル２',
+                heading3: 'サンプル３',
+                headingtext1: 'Mini',
+                headingtext2: 'みに',
+                headingtext3: 'ミニ'
+            },  
+            {
+                coverImage: './assets/img/phone-cover.jpg',
+                name: 'Phone Standard',
+                price: 299,
+                description: 'Not cheap',
+                heading1: 'サンプル１',
+                heading2: 'サンプル２',
+                heading3: 'サンプル３',
+                headingtext1: 'スタンダード',
+                headingtext2: 'Standard',
+                headingtext3: 'すたんだーど'
+            },  
+            {
+                coverImage: './assets/img/phone-cover.jpg',
+                name: 'Phone Special',
+                price: 999,
+                description: 'It is so special.',
+                heading1: 'サンプル１',
+                heading2: 'サンプル２',
+                heading3: 'サンプル３',
+                headingtext1: 'スペシャル',
+                headingtext2: 'スペシャル',
+                headingtext3: 'スペシャル'
+            },  
+        ]
+    }
+
+    async initDb() {
+        await this.cleanDb()
+        this.pushProductsToDb()
+    }
+
+    async cleanDb() {
+        await Product.deleteMany({})
+    }
+
+    pushProductsToDb() {
+        this.products.forEach(
+            (product) => {
+                const newProduct = new Product(product)
+                newProduct.save()
+            }
+        )
+    }
+}
+module.exports = FakeDb
